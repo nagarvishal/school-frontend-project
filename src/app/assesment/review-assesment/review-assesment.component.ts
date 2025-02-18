c2a6cc95e5bb830c71bbf25e58074878f9ee41fd
Vishal Nagar <vishal19.nagar@tataaig.com>
2025-02-18 17:12:05 +0530

    My changes

--- src/app/assesment/review-assesment/review-assesment.component.ts
+++ src/app/assesment/review-assesment/review-assesment.component.ts
@@ -9,6 +9,7 @@ import { Envirment } from '../../../envirment/envirment.variables';
   styleUrl: './review-assesment.component.css'
 })
 export class ReviewAssesmentComponent {
+    abcd:any = ['A','B','C','D','E','F'];
     name: string = "vihsal nagar";
     assesments: any = [];
     studentAssesment: any = [];
@@ -35,6 +36,34 @@ export class ReviewAssesmentComponent {
         this.cassesment = index;
         const url = `/assesment/question/${this.studentAssesment[index].assesment_id}`;
         await this.aservice.generalGetApiCall(this,url,{},"assesmentQA");
+
+    }
+    checkisright(question_id:string,option:string,rightanswer:string){
+        if(rightanswer.split(",").includes(option)){
+            if(this.studentAssesment[this.cassesment].answers[question_id].includes(option)){
+                return 1;
+            }
+            else{
+                return 2;
+            }
+        }
+        else{
+            if(this.studentAssesment[this.cassesment].answers[question_id].includes(option)){
+                return 3;
+            }
+            else{
+                return 4;
+            } 
+        }
+        return 5;
+    }
+
+    checkrightanswer(question_id:string,rightanswer:string)
+    {
+        if(this.studentAssesment[this.cassesment].answers[question_id][0] == rightanswer ){
+            return true;
+        }
+        return false;
     }
 
 }
